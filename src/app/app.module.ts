0f97bafafada28367680237e2ec7e03ec2711e83
Mohamed Sghir <med.sg125@gmail.com>
2024-12-15 18:10:01 +0100

    👌 IMPROVE: Final Solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { OmarComponent } from './omar/omar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent,
+    OmarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/shared/payment-detail.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';  
+import { environment } from '../../environments/environment';
+import { PaymentDetail } from './payment-detail.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+ 
+  url:string = environment.apiBaseUrl + '/PaymentDetails';
+  list:PaymentDetail [] = [];
+  formData : PaymentDetail =new PaymentDetail();
+  constructor(private http:HttpClient) { }
+  refreshList()
+  {
+    this.http.get(this.url).subscribe({
+      next:res=>{
+          this.list = res as PaymentDetail[];
+      },
+      error:err=>{ 
+        console.log(err);
+      }
+    });
+  }
+  postPaymentDetail()
+  {
+    return this.http.post(this.url,this.formData);
+  }
+}
+
